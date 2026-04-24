79b15948a1f181c4427ed88dc5db09485a5d38cf
KM <siukwongman@gmail.com>
2026-04-24 08:59:14 +0800

    revert: remove loading overlay

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <router-view />
-  <!-- Initial loading overlay to hide browser loading bar -->
-  <div v-if="isLoading" class="app-loading-overlay">
-    <div class="loading-content">
-      <a-spin size="large" />
-    </div>
-  </div>
   <a-button
     v-if="showPwaInstallButton"
     type="primary"
@@ -23,16 +17,7 @@ import { ref, onMounted, onUnmounted } from 'vue';
 import { DownloadOutlined } from '@ant-design/icons-vue';
 
 const showPwaInstallButton = ref(false);
-const isLoading = ref(true);
 let deferredPrompt = null;
-
-// Hide browser loading bar during initial load
-window.addEventListener('load', () => {
-  // Delay slightly to ensure everything is ready
-  setTimeout(() => {
-    isLoading.value = false;
-  }, 500);
-});
 
 function handlePwaInstallAvailable(event) {
   deferredPrompt = event.detail;
@@ -71,23 +56,6 @@ onUnmounted(() => {
 </script>
 
 <style>
-.app-loading-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #001529;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 9999;
-}
-
-.loading-content {
-  color: #ffffff;
-}
-
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
